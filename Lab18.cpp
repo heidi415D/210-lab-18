4e10aeda3474680b95ff8e1b4785ca1a76850fff
1080heidi <heidideleon415@gmail.com>
2025-10-05 14:09:41 -0700

    Creating Node Struct

--- Lab18.cpp
+++ Lab18.cpp
@@ -1,1 +1,21 @@
-include <iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node{
+    double rating; // movie rating
+    string comment; // movie comment
+    Node* next; // pointer to next node
+};
+
+Node* head;
+
+void addToHead(double rate, string comm){
+    Node* newNode = new Node; // memory allocation
+    newNode->rating = rate; // set rating
+    newNode->comment = comm; // set comment
+    newNode->next = head; // point new node to current head
+    head = newNode; // update head to new node
+}
+
+int main() {}
